36426c8eddc74da4f3061e1aba6bb35c7b372400
reactexcel <rahul.excel2011@gmail.com>
2019-08-27 14:19:03 +0530

    updated errors

--- routes/scrapper.js
+++ routes/scrapper.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const userController = require('../controllers/userControllers');
+const userController = require('../controllers/scrapControllers');
 
 router.post('/flipkart/mobile', userController.fetchData);
 
