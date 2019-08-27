d8ce3732646b9e855dc7864710cca7f19583a292
reactexcel <rahul.excel2011@gmail.com>
2019-08-27 17:52:00 +0530

    updated Question No. 4.2

--- controllers/scrapControllers.js
+++ controllers/scrapControllers.js
@@ -8,11 +8,11 @@ let fetchMobiles = async (req, res, next) => {
             const $ = cheerio.load(html);
             const wikiName = [];
             $('._2kSfQ4').each(function (i, elem) {
-                wikiName[i] = {
+                wikiName.push({
                     name: $(this).find($('.iUmrbN')).text(),
                     price: $(this).find($('._3o3r66')).text(),
                     specs: $(this).find($('.BXlZdc')).text()
-                };
+                });
             })
             res.send(wikiName);
         })
@@ -28,11 +28,11 @@ let fetchShirts = async (req, res) => {
             const $ = cheerio.load(html);
             const wikiName = [];
             $('.product-desc-rating').each(function (i, elem) {
-                wikiName[i] = {
+                wikiName.push({
                     name: $(this).find($('.product-title')).text(),
                     maxPrice: $(this).find($('.product-desc-price')).text(),
                     discountPrice: $(this).find($('.product-price')).text()
-                };
+                });
             })
             res.send(wikiName);
         })
