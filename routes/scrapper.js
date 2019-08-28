2ff99889fc2c3e3c541ea66f574814fecb80212e
reactexcel <rahul.excel2011@gmail.com>
2019-08-28 17:39:55 +0530

    added Question 4.3

--- routes/scrapper.js
+++ routes/scrapper.js
@@ -6,4 +6,6 @@ router.post('/flipkart/mobile', scrapController.fetchMobiles);
 
 router.post('/snapdeal/mobile', scrapController.fetchShirts);
 
+router.post('/flipkart/mobile/full', scrapController.fetchAllMobiles);
+
 module.exports = router;
